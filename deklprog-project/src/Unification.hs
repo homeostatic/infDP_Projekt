d4568cfde07fa364dadfd151e96a82a97aaed4ca
Sam <Sam@dontemail.me>
2025-01-24 14:49:59 +0100

    updated with PA answers

--- deklprog-project/src/Unification.hs
+++ deklprog-project/src/Unification.hs
@@ -7,54 +7,41 @@ module Unification
 
 
 import Data.Maybe
-
-import Test.QuickCheck
-
---helper function
--- firstJust
+import Subst
+import Vars
+import Base.Type
 
 
--- die die Unstimmigkeitsmenge zweier Terme berechnet und sie als Paar zurückgibt.
--- 
--- Sollte die Unstimmigkeitsmenge leer sein, soll die Funktion stattdessen Nothing zurückgeben.
--- ds :: Term -> Term -> Maybe (Term, Term)
--- --Var mit Var
--- ds (Var t1) (Var t2)      | t1 == t2   = Nothing
---                           |otherwise   = Just (Var n1,Var n)
--- --Var mit Comb
--- ds (Comb c1 ts)@t1 (Var v2)            = Just (Var v2, t1)
--- ds (Var v1) (Comb c2 ts)@t2            = Just (Var v1, t2)
--- --Comb mit Comb
--- ds (Comb c0 [])@t0 (Comb c1 [])@t1    = if c0==c1 then Nothing else Just(t0,t1)
--- ds (Comb c0 ts0)@t0 (Comb c1 ts1)@t1  = if c0 == c1 and length(ts0) == length(ts1) 
---                                             then first(filter (\= Nothing (concatMap (\(x0, x1)->ds x0 x1)(zip ts0 ts1)))
---                                             else Just(t0, t1)
-
-
-
-
---allgemeinsten Unifikator für zwei Terme bestimmt, sofern die beiden Terme unifizierbar sind.
--- Ansonsten soll die Funktion Nothing zurückgeben.
--- --                        Just [(var, term)]
--- unify :: Term -> Term -> Maybe Subst
--- -- unify (Var v0) (Var v1) = Just Subst [(VarName v0, v1)]
--- -- unify t1 t2 = ds (apply t1 t2 
--- unify t0 t1 = unify' empty t0 t1
---   where
---     unify' s t0' t1' = case (ds (apply s t0') (apply s t1')) of
---       Nothing -> Just s
---       Just(Var x, t) | not(occurs x t) = unify' (compose (single Var x t) s) t0' t1'
---       _ -> Nothing 
-
-
-  
--- Properties
-
-{- Uncomment this to test the properties when all required functions are implemented
+import Test.QuickCheck ( quickCheckAll, (==>), Property )
 
 -- Does  a variable occur in a term?
 occurs :: VarName -> Term -> Bool
 occurs v t = v `elem` allVars t
+
+-- Disagreementset -> zipWith - Funktion
+ds :: Term -> Term -> Maybe(Term, Term) 
+ds (Var v0) (Var v1) = if v0 == v1 then Nothing else Just( Var v0, Var v1)
+ds t@(Comb c0 ts) (Var v1) = Just( Var v1, t)
+ds (Var v0) t@(Comb c1 ts) = Just(Var v0, t) 
+ds t0@(Comb c0 []) t1@(Comb c1 []) = if c0 == c1 then Nothing else Just(t0,t1)
+ds t0@(Comb c0 ts0) t1@(Comb c1 ts1) = if ((c0 == c1) && length ts0 == length ts1) then let nds = (filter (/= Nothing) (map (\(x0, x1) -> ds x0 x1) (zip ts0 ts1))) in 
+  (if nds == [] then Nothing else head nds)
+  else Just(t0, t1)
+
+-- Unifikation
+unify :: Term -> Term -> Maybe Subst 
+unify t0 t1 = unify' empty t0 t1 
+  where 
+    unify' s t0' t1' = case (ds (apply s t0') (apply s t1')) of 
+      Nothing -> Just s 
+      Just(Var x, t) | not(occurs x t) -> unify' (compose (single x t) s) t0' t1' 
+      _ -> Nothing
+
+
+
+-- Properties
+
+
 
 -- Properties
 
@@ -84,7 +71,3 @@ return []
 -- Run all tests
 testUnification :: IO Bool
 testUnification = $(quickCheckAll)
--}
-
-
-
