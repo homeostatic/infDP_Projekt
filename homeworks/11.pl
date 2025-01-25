09be8c81389eb32e5298c2281b6990a976ec64a1
haudom <56408760+haudom@users.noreply.github.com>
2025-01-25 18:49:28 +0100

    Aufgabe 3 erledigt

--- homeworks/11.pl
+++ homeworks/11.pl
@@ -1,11 +1,12 @@
 %Aufgabe 2
 
-%Lösung von DeepSeek
+%von DeepSeek
 /* horseman(Heads, Feet, Men, Horses) :-
-    add(Men, Horses, Heads),
-    mult(Men, s(s(o)), LegsMen),
-    mult(Horses, s(s(s(s(o)))), LegsHorses),
-    add(LegsMen, LegsHorses, Feet). */
+    add(Men, Horses, Heads),          % Zuerst Köpfe einschränken
+    mult(Men, s(s(o)), MFeet),        % Dann Beine berechnen
+    mult(Horses, s(s(s(s(o)))), HFeet),
+    add(MFeet, HFeet, Feet).          % Abschließend Beinsumme prüfen
+     */
 
 
 
@@ -40,4 +41,28 @@ horseman(Heads, Feet, Men, Horses) :-
     
  % ?- horseman(s(s(s(s(s(s(s(s(o)))))))), s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(s(o)))))))))))))))))))), M, H).
 
- 
+
+
+ % Aufgabe 3
+fisch(Gewicht) :- Gewicht >= 0.
+bruecke(Mobile1, Mobile2).
+
+gewicht(Mobile, Gewicht) :- Mobile = fisch(Gewicht).
+gewicht(Mobile, Gewicht) :- Mobile = bruecke(Mobile1, Mobile2), gewicht(Mobile1, G1), gewicht(Mobile2, G2), Gewicht is G1 + G2 + 1.
+
+ausgeglichen(fisch(_)).
+ausgeglichen(bruecke(Mobile1,Mobile2)) :- gewicht(Mobile1, Gewicht),  gewicht(Mobile2, Gewicht), ausgeglichen(Mobile1), ausgeglichen(Mobile2).
+
+mobile(Mobile,Gewicht) :- ausgeglichen(Mobile), gewicht(Mobile,Gewicht).
+
+%Was liefern die folgenden Anfragen?
+%| ?- mobile(bruecke(fisch(1),fisch(1)),2).
+%       false.
+
+%| ?- mobile(bruecke(fisch(1),fisch(1)),3).
+%       true.
+
+%| mobile(X,3).
+% Es kommt ein Fehler. Das liegt daran, dass is eine partielles prädikat ist. 
+% Bei der Auswertung wird die 2. Regel von Gewicht sind Mobile1 und Mobile2 unbelegt. 
+% Dadurch wird der is Operator mit zwei unbelegten Variablen aufgerufen, und es kommt zu der Fehlermeldung
